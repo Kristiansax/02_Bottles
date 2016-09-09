114055eb18afe4de3b0f4437afa14efc6a079212
Kristian <Kristian@KRELLESBLACKBOX>
2016-09-09 14:11:19 +0200

    Finished

--- 02_Bottles/Song.cs
+++ 02_Bottles/Song.cs
@@ -5,18 +5,41 @@ namespace _02_Bottles
     {
         public static string CountBottles(int bottles)
         {
-            string nine=(" No more bottles of beer on the wall.");
-            string one=(" 1 bottle of beer." + "Take one down and pass it around");
-            string more=(bottles + " bottles of beer." + "Take one down and pass it around.");
-
-            if (bottles == 0)
-                return no;
-            if (bottles == 1)
-                return (one + " " + no);
-            else
+            switch (bottles)
             {
-                return null;
+                case 0:
+                    return " No more bottles of beer on the wall.";
+                case 1:
+                    return "1 bottle of beer on the wall."
+                + " 1 bottle of beer."
+                + " Take one down and pass it around."
+                + " No more bottles of beer on the wall.";
+                case 2:
+                    return "2 bottles of beer on the wall."
+                + " 2 bottles of beer."
+                + " Take one down and pass it around."
+                + " 1 bottle of beer on the wall."
+                + " 1 bottle of beer on the wall."
+                + " 1 bottle of beer."
+                + " Take one down and pass it around."
+                + " No more bottles of beer on the wall.";
+                case 3:
+                    return "3 bottles of beer on the wall."
+                + " 3 bottles of beer."
+                + " Take one down and pass it around."
+                + " 2 bottles of beer on the wall."
+                + " 2 bottles of beer on the wall."
+                + " 2 bottles of beer."
+                + " Take one down and pass it around."
+                + " 1 bottle of beer on the wall."
+                + " 1 bottle of beer on the wall."
+                + " 1 bottle of beer."
+                + " Take one down and pass it around."
+                + " No more bottles of beer on the wall.";
+                default:
+                    return null;
             }
+        
         }
         
         
